d53c5614b17096290b3b5328b1d8c07fa23cfa7c
rlarabi <larabi.radouan@gmail.com>
2023-03-25 18:09:33 +0000

    cheking qotes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rlarabi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:11:37 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/03/24 11:41:51 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/03/25 16:08:07 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+# define WORD 1
+# define DASH 2
+# define SPACE 3
+# define DOUBLE_Q 4
+# define SINGLE_Q 5
+# define RED_IN 6
+# define RED_OUT 7
+# define HERDOC 8
+# define APPE 9
+# define PIPE 10
+# define ENV 11
+
 typedef struct s_cmd
 {
     char *content;
--- parsing/main.c
+++ parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: rlarabi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:00:22 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/03/24 17:41:35 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/03/25 16:58:25 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,52 +49,33 @@ void    ft_lstadd_back(t_command **lst,t_command *new)
         new->prev = temp;
 	}
 }
-// void    set_type_token(t_command *tmp, char *str,int *i)
-// {
-//     if (str[*i] == '-')
-//     {
-//         tmp->len = 1;
-//         tmp->type = 2;
-//         *i++;
-//     }
-// }
-void displayList(t_command *node)
+void displayList(t_command **node)
 {
-    while (node != NULL) {
-        printf("%s -> %d\n", node->content, node->state);
-        node = node->next;
+    while ((*node) != NULL) {
+        printf("%s -> %d\n", (*node)->content, (*node)->type);
+        (*node) = (*node)->next;
     }
     printf("\n");
 }
 
-void    check_close_qotes(t_command **cmd)
+void    check_close_qotes(char *str, char *string_perfect)
 {
-    t_command *tmp;
-    int start_dc = 0;
-    int start_sc = 0;
     int i;
+    int j;
 
-
+    j = 0;
     i = 0;
-    tmp = *cmd;
-    while(tmp != NULL)
+    while(str[i])
     {
-        i = 0;
-        while(tmp->content[i])
-        {
-            if (tmp->content[i] == '\"')
-                start_dc++;
-            if (tmp->content[i] == '\'')
-                start_sc++;
-            i++;
-        }
-        tmp = tmp->next;
+        if (str[i] == string_perfect[j])
+                j++;
+        i++;
+        if (j == 2)
+            j = 0;
+        
     }
-    if (start_dc % 2 || start_sc % 2)
-    {
-        printf("Error qotes %d %d\n", start_dc, start_sc);
-        exit(0);
-    } 
+    if (j)
+        printf("Error syntaxe\n");
 }
 void    set_state(t_command **cmd)
 {
@@ -109,39 +90,93 @@ void    set_state(t_command **cmd)
         i = 0;
         while(tmp->content[i])
         {
-            tmp->state = 2;
+            tmp->state = 1;
             if (tmp->content[i] == '\"')
             {
-                tmp->state = 3;
-                tmp = tmp->next;
-                count++;
-                close_dc = 1;
-                if (count == 2)
-                {
-                    close_dc = 0;
-                    count = 0;
-                }
-                if (close_dc == 1)
-                    tmp->state = 1;
-            }
-            else if (tmp->content[i] == '\'')
-            {
-                // tmp->state = 4;
-                // tmp = tmp->next;
-                // count++;
-                // close_dc = 1;
-                // if (count == 2)
-                // {
-                //     close_dc = 0;
-                //     count = 0;
-                // }
-                // if (close_dc == 1)
-                //     tmp->state = 1;
+                while(tmp->content[i] != '\"');
             }
             i++;
         }
         tmp = tmp->next;
     }
+}
+int    fill_types(t_command *tmp, char c, int *i, char *str)
+{
+    if (c == '-')
+    {
+        tmp->len = 1;
+        tmp->type = DASH;
+        (*i)++;
+    }
+    else if (ft_isalpha(c))
+    {
+        while (ft_isalpha(str[*i]))
+        {
+            tmp->len++;
+            (*i)++;
+        }
+        tmp->type = WORD;
+    }
+    else if (c == ' ')
+    {
+        tmp->len = 1;
+        tmp->type = SPACE;
+        (*i)++;
+    }
+    else if (c == '\"')
+    {
+        tmp->len = 1;
+        tmp->type = DOUBLE_Q;
+        (*i)++;
+    }
+    else if (c == '\'')
+    {
+        tmp->len = 1;
+        tmp->type = SINGLE_Q;
+        (*i)++;
+    }
+    else if (c == '<' && str[(*i) + 1] != '<')
+    {
+        tmp->len = 1;
+        tmp->type = RED_IN;
+        (*i)++;
+    }
+    else if (c == '>' && str[(*i) + 1] != '>')
+    {
+        tmp->len = 1;
+        tmp->type = RED_OUT;
+        (*i)++;
+    }
+    else if (c == '>' && str[(*i) + 1] == '>')
+    {
+        tmp->len = 2;
+        tmp->type = APPE;
+        (*i) += 2;
+    }
+    else if (c == '<' && str[(*i) + 1] == '<')
+    {
+        tmp->len = 2;
+        tmp->type = HERDOC;
+        (*i) += 2;
+    }
+    else if (c == '|')
+    {
+        tmp->len = 1;
+        tmp->type = PIPE;
+        (*i)++;
+    }
+    else if (c == '$')
+    {
+        tmp->len = 1;
+        tmp->type = ENV;
+        (*i)++;
+    }
+    else
+    {
+        printf("error token");
+        return 0;
+    }
+    return 1;
 }
 
 int main(int ac, char **av, char **env)
@@ -151,7 +186,6 @@ int main(int ac, char **av, char **env)
     int len;
     t_command *cmd;
     t_command *tmp;
-    // cmd = init_cmd();
     while(1)
     {
         i = 0;
@@ -160,87 +194,15 @@ int main(int ac, char **av, char **env)
         while(str[i])
         {
             tmp = init_cmd();
-            if (str[i] == '-')
-            {
-                tmp->len = 1;
-                tmp->type = 2;
-                i++;
-            }
-            else if (ft_isalpha(str[i]))
-            {
-                while (ft_isalpha(str[i]))
-                {
-                    tmp->len++;
-                    i++;
-                }
-                tmp->type = 1;
-            }
-            else if (str[i] == ' ')
-            {
-                tmp->len = 1;
-                tmp->type = 3;
-                i++;
-            }
-            else if (str[i] == '\"')
-            {
-                tmp->len = 1;
-                tmp->type = 4;
-                i++;
-            }
-            else if (str[i] == '\'')
-            {
-                tmp->len = 1;
-                tmp->type = 5;
-                i++;
-            }
-            else if (str[i] == '<' && str[i + 1] != '<')
-            {
-                tmp->len = 1;
-                tmp->type = 6;
-                i++;
-            }
-            else if (str[i] == '>' && str[i + 1] != '>')
-            {
-                tmp->len = 1;
-                tmp->type = 7;
-                i++;
-            }
-            else if (str[i] == '>' && str[i + 1] == '>')
-            {
-                tmp->len = 2;
-                tmp->type = 8;
-                i += 2;
-            }
-            else if (str[i] == '<' && str[i + 1] == '<')
-            {
-                tmp->len = 2;
-                tmp->type = 9;
-                i += 2;
-            }
-            else if (str[i] == '|')
-            {
-                tmp->len = 1;
-                tmp->type = 10;
-                i++;
-            }
-            else if (str[i] == '$')
-            {
-                tmp->len = 1;
-                tmp->type = 11;
-                i++;
-            }
-            else
-            {
-                printf("error token");
+            if (!fill_types(tmp, str[i], &i,str))
                 return 0;
-            }
             tmp->content = ft_substr(str, i - tmp->len, tmp->len);
             ft_lstadd_back(&cmd, tmp);
-            // printf("%s   -> %d -> %d -> %d\n", tmp->content, i - tmp->len , tmp->len);
         }
-        check_close_qotes(&cmd);
-        set_state(&cmd);
-        displayList(cmd);
+        check_close_qotes(str, "\"\"");
+        check_close_qotes(str, "\'\'");
+        // set_state(&cmd);
+        displayList(&cmd);
         free(str);
     }
     return 0;

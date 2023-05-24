597c568f4f94bacdf4f32fb98b9740f327a694b8
Hassan Lakhal <hlakhal-@e1r10p2.1337.ma>
2023-05-24 18:41:41 +0100

    fixed error heap-use-after-free

--- excution/builtins/unset.c
+++ excution/builtins/unset.c
@@ -3,14 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rlarabi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:36:28 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/05/21 19:55:21 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/05/24 18:40:05 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+t_env	*duplicate_linked_list(t_env *head)
+{
+	t_env	*new_head;
+	t_env	*current_original;
+	t_env	*current_duplicate;
+	t_env	*new_node;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new_head = (t_env *)malloc(sizeof(t_env));
+	new_head->var = ft_strdup(head->var);
+	new_head->value = ft_strdup(head->value);
+	new_head->flag = 0;
+	new_head->next = NULL;
+	current_original = head->next;
+	current_duplicate = new_head;
+	while (current_original != NULL)
+	{
+		new_node = (t_env *)malloc(sizeof(t_env));
+		new_node->var = ft_strdup(current_original->var);
+		new_node->value = ft_strdup(current_original->value);
+		new_node->flag = 0;
+		new_node->next = NULL;
+		current_duplicate->next = new_node;
+		current_original = current_original->next;
+		current_duplicate = current_duplicate->next;
+	}
+	return (new_head);
+}
 
 void	remove_node(t_env **head_ref, char *key, char *key1)
 {
@@ -18,7 +50,8 @@ void	remove_node(t_env **head_ref, char *key, char *key1)
 	t_env	*prev;
 
 	temp = *head_ref;
-	if (temp != NULL && temp->var == key && temp->value == key1)
+	if (temp != NULL && !ft_strcmp(temp->var, key) && !ft_strcmp(temp->value,
+			key1))
 	{
 		*head_ref = temp->next;
 		free(temp->value);
@@ -26,8 +59,10 @@ void	remove_node(t_env **head_ref, char *key, char *key1)
 		free(temp);
 		return ;
 	}
-	while (temp != NULL && (temp->var != key || temp->value != key1))
+	while (temp != NULL && (ft_strcmp(temp->var, key) || ft_strcmp(temp->value,
+				key1)))
 	{
+		//printf("%s\t%s\n",key,key1);
 		prev = temp;
 		temp = temp->next;
 	}
@@ -57,13 +92,14 @@ int	ft_unset(t_cmd_line **commands_v)
 			ft_putendl_fd(" : not a valid identifier", 1);
 			g_gv->exit_status = 1;
 		}
-		temp = g_gv->env;
+		temp = duplicate_linked_list(g_gv->env);
 		while (temp)
 		{
 			if (var && !ft_strcmp(temp->var, var))
 				remove_node(&g_gv->env, temp->var, temp->value);
 			temp = temp->next;
 		}
+		//free()
 	}
 	return (0);
 }
